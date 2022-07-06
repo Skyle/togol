e81aa7bf512d9a0b78186586cc3db69fdc4637d3
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-07-06 20:39:41 +0200

    refactored input into custom Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
 
-type Props = {};
+type Props = {
+  value: string | number | readonly string[] | undefined;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  dispatchTodoItem: any;
+};
 
-function Input({}: Props) {
-  return <div>Input</div>;
+function Input(props: Props) {
+  const { value, setValue, dispatchTodoItem } = props;
+
+  return (
+    <input
+      value={value}
+      className='border border-neutral-300 shadow shadow-neutral-200 rounded-lg bg-neutral-50 focus:outline-none px-4 py-2'
+      type='text'
+      onChange={(e) => setValue(e.target.value)}
+      onKeyUp={(e) => {
+        if (e.key === 'Enter') {
+          dispatchTodoItem();
+        }
+      }}
+    />
+  );
 }
 
 export default Input;
