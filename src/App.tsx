f53e3ad5461c9c7276d594b4dc9033e8683a535a
Philipp Friedrich <freezer05@googlemail.com>
2022-07-06 11:43:50 +0200

    design erster prototype

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ function App() {
       <header className="py-4">
         <h1 className="text-4xl text-center tracking-widest">Togol</h1>
       </header>
+      <main className="flex justify-center">
+        <div className="flex space-x-2">
+          <input
+            className="border shadow rounded-lg focus:outline-none px-4 py-2"
+            type="text"
+          />
+          <button className="px-4 shadow py-2 border rounded-lg">+</button>
+        </div>
+      </main>
     </div>
   );
 }
