8e9f64b509152959ed62321920952ffc2c27c80a
Philipp Friedrich <freezer05@googlemail.com>
2022-07-06 11:38:02 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header className="py-4">
+        <h1 className="text-4xl text-center tracking-widest">Togol</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
