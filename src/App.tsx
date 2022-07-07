f72ad1f6b343de03179ed55d87b4158c76fcf5e6
Philipp Friedrich <freezer05@googlemail.com>
2022-07-07 10:54:37 +0200

    zitrone

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       <header className="py-4">
         <h1 className="text-4xl text-center tracking-widest font-serif">
-          Togol 🍋
+          Zitrone 🍋
         </h1>
       </header>
       <main className="flex justify-center px-4">
