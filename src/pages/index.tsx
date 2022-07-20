7319e27f0aee026efccc3b0e66c5e021ebd34d93
Philipp Friedrich <freezer05@googlemail.com>
2022-07-20 19:08:42 +0200

    useranzeiger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
-import { TodoInput } from '../components/TodoInput';
-import { Todos } from '../components/Todos';
+import { TodoInput } from "../components/TodoInput";
+import { Todos } from "../components/Todos";
 
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
-import { LogoutComponent } from '../components/user/LogoutComponent';
+import { LogoutComponent } from "../components/user/LogoutComponent";
 
 export const IndexPage = () => {
   const localTodoList = useSelector(
     (state: RootState) => state.localTodos.todos
   );
+  const userState = useSelector((state: RootState) => state.user);
 
   return (
-    <div className='grid gap-4 max-w-lg'>
+    <div className="grid gap-4 max-w-lg">
       <TodoInput />
+      {userState.name && (
+        <div className="font-light">Hallo, {userState.name}!</div>
+      )}
       <Todos />
-
       <LogoutComponent />
 
       {/* <div>{JSON.stringify(localTodoList)}</div> */}
--- src/services/userSlice.ts
+++ src/services/userSlice.ts
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
-import type { RootState } from '../store';
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
 
 interface UserState {
   id: string | null;
@@ -8,24 +8,43 @@ interface UserState {
   token: string | null;
 }
 
+function parseJwt(token: string) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
 const initialState: UserState = {
   id: null,
   name: null,
-  token: localStorage.getItem('token'),
+  token: localStorage.getItem("token"),
 };
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
 
   initialState,
   reducers: {
     setToken: (state, action: PayloadAction<string>) => {
+      localStorage.setItem("token", action.payload);
       state.token = action.payload;
-      localStorage.setItem('token', action.payload);
+      const decryptedJWT = parseJwt(action.payload);
+      state.name = decryptedJWT.name;
+      state.id = decryptedJWT.id;
     },
     logout: (state) => {
       state.token = null;
-      localStorage.removeItem('token');
+      localStorage.removeItem("token");
     },
   },
 });
